eb464b71ee64f41a7c78edcc2cba036247cd63a4
Akhil-star <akhilakki1305@gmail.com>
2020-09-26 16:18:27 +0530

    Equals Method

--- src/main/java/com/LineComparsion1/App.java
+++ src/main/java/com/LineComparsion1/App.java
@@ -8,14 +8,33 @@ public class App
 {
     public static void main( String[] args )
     {
-    	int x1,x2,y1,y2;
-		int  l1;
+    	  int x1,x2,y1,y2;
+    	  int x3,y3,x4,y4;
+		  int  l1,l2;
 		
-		x1=1; y1=1;
-		x2=4;y2=4;
+		  x1=1; y1=1;
+		  x2=4;y2=4;
 		
-		l1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		  x3=5;y3=1;
+		  x4=6;y4=1;
 		
-		System.out.println("Length of ("+x1+","+y1+") and ("+x2+","+y2+") is "+l1);
+		  l1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		  l2=(int)Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+		  System.out.println("Length of ("+x1+","+y1+") and ("+x2+","+y2+") is "+l1);
+		  System.out.println("Length of ("+x3+","+y3+") and ("+x4+","+y4+") is "+l2);
+		  
+		  Integer a=new Integer(l1);
+          Integer b=new Integer(l2);
+          
+		  if(a.equals(b))
+		  {
+			System.out.println("Lengths are Equal");
+		  }
+		  else
+		  {
+			System.out.println("Lengths are Not Equal");
+
+		  }
+		 
     }
 }
