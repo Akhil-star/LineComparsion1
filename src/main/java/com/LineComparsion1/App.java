5e64cf1dee243a16457c827c018260c291cf4c70
Akhil-star <akhilakki1305@gmail.com>
2020-09-26 16:07:55 +0530

    Length of 2 points

--- src/main/java/com/LineComparsion1/App.java
+++ src/main/java/com/LineComparsion1/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Weclome to Line Comparison Computation Program " );
+    	int x1,x2,y1,y2;
+		int  l1;
+		
+		x1=1; y1=1;
+		x2=4;y2=4;
+		
+		l1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		
+		System.out.println("Length of ("+x1+","+y1+") and ("+x2+","+y2+") is "+l1);
     }
 }
