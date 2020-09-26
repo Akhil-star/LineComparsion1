ab8b0f52375f88381c4b84825b0fec36ce394798
Akhil-star <akhilakki1305@gmail.com>
2020-09-26 16:49:48 +0530

    using class methods

--- src/main/java/com/LineComparsion1/App.java
+++ src/main/java/com/LineComparsion1/App.java
@@ -18,10 +18,30 @@ public class App
 		
 	    l1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 	    l2=(int)Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+	    
+	    
 	    System.out.println("Length of ("+x1+","+y1+") and ("+x2+","+y2+") is "+l1);
 	    System.out.println("Length of ("+x3+","+y3+") and ("+x4+","+y4+") is "+l2);
+	    
+	    equals(l1,l2);
+	    compare(l1,l2);
+   
+    }
+    
+    public static void equals(int l1,int l2) {
+		 Integer a=new Integer(l1);
+		 Integer b=new Integer(l2);
 		
-	    Integer a=new Integer(l1);
+         if(a.equals(b))    
+         System.out.println("Lengths are Equal");
+         else
+		 System.out.println("Lengths are Not Equal");
+    }	
+			
+
+	 public static int compare(int l1, int l2) {
+	   
+		Integer a=new Integer(l1);
         Integer b=new Integer(l2);
         int compare=a.compareTo(b);
         
@@ -37,6 +57,7 @@ public class App
         {
       	  System.out.println("l1 is equal to l2");
         }
+        return compare;
 		
     }
 	
